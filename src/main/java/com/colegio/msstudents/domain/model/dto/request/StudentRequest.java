34bc9020797c94ec8f7c247aa11acf37bc5c86df
daniel <daniel.lizana01@gmail.com>
2024-05-16 21:18:27 -0500

    dr

--- src/main/java/com/colegio/msstudents/domain/model/dto/request/StudentRequest.java
+++ src/main/java/com/colegio/msstudents/domain/model/dto/request/StudentRequest.java
@@ -1,10 +1,12 @@
 package com.colegio.msstudents.domain.model.dto.request;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,9 +14,22 @@ import lombok.Setter;
 @Setter
 public class StudentRequest {
 
+
     private String id;
+
+    @NotNull
+    @NotBlank
     private String name;
+
+    @NotNull
+    @NotBlank
     private String lastName;
+
+    @NotNull
+    @NotBlank
     private String status;
+
+
+    @Min(4)
     private int age;
 }
--- src/main/java/com/colegio/msstudents/infraestructure/rest/controller/StudentsController.java
+++ src/main/java/com/colegio/msstudents/infraestructure/rest/controller/StudentsController.java
@@ -4,6 +4,9 @@ package com.colegio.msstudents.infraestructure.rest.controller;
 import com.colegio.msstudents.application.usecases.StudentService;
 import com.colegio.msstudents.domain.model.dto.StudentDto;
 import com.colegio.msstudents.domain.model.dto.request.StudentRequest;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -20,15 +23,26 @@ public class StudentsController {
         this.studentService = studentService;
     }
 
-    @PostMapping
+    @PostMapping(produces = "application/json", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Void> createStudent(@RequestBody StudentRequest student) {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "usuario registrado correctamente"),
+            @ApiResponse(responseCode = "400",description = "Solicitud incorrecta"),
+            @ApiResponse(responseCode = "500",description = "error en el servidor")
+    })
+    public Mono<Void> createStudent(@Valid @RequestBody StudentRequest student) {
         return studentService.createNewStudent(student);
     }
 
-    @GetMapping("/activos")
+    @GetMapping(value = "/{status}",produces = "application/json", consumes = "application/json")
     @ResponseStatus(HttpStatus.FOUND)
-    public Flux<StudentDto> getStudentActives() {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "usuario registrado correctamente"),
+            @ApiResponse(responseCode = "400",description = "Solicitud incorrecta"),
+            @ApiResponse(responseCode = "500",description = "error en el servidor")
+    })
+    public Flux<StudentDto> getStudentActives(@Valid @PathVariable String status) {
+        System.out.println(status);
         return studentService.getAllStudentStatusActive();
     }
 
